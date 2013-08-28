05b7c8fe6e19bc8748ba836a9fef89331fcfb412
Jeff Sutton <jeff.sutton@simplestream.com>
2013-08-28 14:41:46 +0100

    Added space in message

--- src/com/loopj/android/http/AsyncHttpRequest.java
+++ src/com/loopj/android/http/AsyncHttpRequest.java
@@ -124,7 +124,7 @@ class AsyncHttpRequest implements Runnable {
                 // there's a bug in HttpClient 4.0.x that on some occasions causes
                 // DefaultRequestExecutor to throw an NPE, see
                 // http://code.google.com/p/android/issues/detail?id=5255
-                cause = new IOException("NPE in HttpClient" + e.getMessage());
+                cause = new IOException("NPE in HttpClient " + e.getMessage());
                 retry = retryHandler.retryRequest(cause, ++executionCount, context);
             }
         }
